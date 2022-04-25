3c7d3ad55a260b051f91e905ff29fa3e810e5f6a
nesteagle <aaronw9810@gmail.com>
2022-04-24 19:18:53 -0700

    canvas

---
+++ html/draw.ts
@@ -0,0 +1,3 @@
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+const context = canvas.getContext("2d") as CanvasRenderingContext2D;
+context.fillRect(canvas.clientWidth/2,canvas.clientHeight/2,100,100)
