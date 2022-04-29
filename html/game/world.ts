1f0150c50c7bb93404405c46648049690d554084
nesteagle <aaronw9810@gmail.com>
2022-04-28 21:10:50 -0700

    fix

--- html/game/world.ts
+++ html/game/world.ts
@@ -6,7 +6,7 @@
 //     console.log(e.detail.name,e.detail.attack);
 
 // }) as EventListener);   event framework
-import {MeleeWarrior} from "./entity"
+import {MeleeWarrior} from "./entity";
 let canvas=document.getElementById("canvas") as HTMLCanvasElement;
 let context=canvas.getContext("2d") as CanvasRenderingContext2D;
 let entities:Array<any>=[new MeleeWarrior(0,100,"left"),new MeleeWarrior(800,100,"right")];
@@ -14,14 +14,15 @@ function tick(){
     for (let units in entities){
         let currentUnit=entities[units]
         for (let j:number=0;j<entities.length;j++){
-            entities[j].draw()
-            entities[j].move()
+            entities[j].draw();
+            entities[j].move();
             if (Math.abs(currentUnit.x-entities[j].x)<=currentUnit.range && entities[j]!==currentUnit){
                 //handle class type- aka Melee type or Ranged type
+                console.log("attackable",currentUnit); //debug
             }
             if (entities[j].x<-25||entities[j].x>canvas.width+25){
                 delete entities[j];
-                console.log("deleted")
+                console.log("deleted");
             }
         }
     };
@@ -29,7 +30,7 @@ function tick(){
 function update():void{
     window.requestAnimationFrame(update);
     context.clearRect(0, 0, canvas.width, canvas.height);
-    tick()
+    tick();
 }
 update();
 
